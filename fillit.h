521e8d412a864f8210c2ce4ca5e6e45003179648
Jarvis Nederlof <jarvisnederlof@gmail.com>
2017-03-23 13:43:22 -0700

    committing fillit.h header file

---
+++ fillit.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnederlo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/23 13:39:59 by jnederlo          #+#    #+#             */
+/*   Updated: 2017/03/23 13:40:11 by jnederlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# define BUF_SIZE 1024
+
+
+#endif
