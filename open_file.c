1e53ec39799fd8976fe63fdc6ec8b4c4c89a4107
Jarvis Nederlof <jarvisnederlof@gmail.com>
2017-03-23 13:38:25 -0700

    adding example file and open_file program

---
+++ open_file.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open_file.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnederlo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/22 19:16:51 by jnederlo          #+#    #+#             */
+/*   Updated: 2017/03/22 19:17:00 by jnederlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// need to compile with ft_putstr.c, ft_putchar.c, ft_strnew.c, ft_bzero.c
+// BUF_SIZE is defined in the header as 1024.
+
+#include "libft.h"
+
+int	main()
+{
+	int		fd;
+	char	*buf;
+	size_t	read_bytes;
+
+	fd = open("example_fillit", O_RDONLY);
+	if (fd == -1)
+		ft_putstr("error\n"); //this is probably wrong.
+	buf = ft_strnew(BUF_SIZE);
+	ft_bzero(buf, BUF_SIZE);
+	read_bytes = read(fd, buf, BUF_SIZE);
+	while (*buf)
+	{
+		write (1, buf, 1);
+		buf++;
+	}
+	close(fd); //need to add error checking.
+	return (0);
+}
