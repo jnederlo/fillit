e1448f8bf11026cc04e1f8d4e22d1448b7d53673
Jarvis <jarvisnederlof@Jarvis.local>
2017-03-24 17:25:19 -0700

    seperated out functions from main and fixed Makefile

--- fillit/main.c
+++ fillit/main.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   open_file.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jnederlo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,12 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// need to compile with ft_putstr.c, ft_putchar.c, ft_strnew.c, ft_bzero.c
+// BUF_SIZE is defined in the header as 1024.
+
 #include "fillit.h"
 
-int	open_file(char *file)
+int	main(int argc, char **argv)
 {
-	int	fd;
-	
-	fd = open(file, O_RDONLY);
-	return (fd);
+	char	*file;
+	int		fd;
+	char	*buf;
+	int		bytes_read;
+	int		close_return;
+
+	if (argc != 2)
+		return (0);
+	file = argv[1];
+	fd = open_file(file);
+	if (fd == -1)
+	{
+		ft_putstr("error\n");
+		return (0);
+	}
+	buf = ft_strnew(BUF_SIZE);
+	bytes_read = read_file(fd, buf);
+	if (bytes_read == -1)
+	{
+		ft_putstr("error\n");
+		return (0);
+	}
+	while (*buf)
+	{
+		write (1, buf, 1);
+		buf++;
+	}
+	close_return = close(fd);
+	if (close_return == -1)
+	{
+		ft_putstr("File Did Not Close\n");
+		return (0);
+	}
+	return (0);
 }
--- fillit/read_file.c
+++ fillit/read_file.c
@@ -1,20 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   read_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erintala <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jnederlo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/23 15:25:33 by erintala          #+#    #+#             */
-/*   Updated: 2017/03/23 15:25:34 by erintala         ###   ########.fr       */
+/*   Created: 2017/03/22 19:16:51 by jnederlo          #+#    #+#             */
+/*   Updated: 2017/03/22 19:17:00 by jnederlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
-{
-	int fd;
-	char *buf;
-	int ret;
+#include "fillit.h"
 
-	return (0);
+int	read_file(int fd, char *buf)
+{
+	int	read_bytes;
+
+	read_bytes = read(fd, buf, BUF_SIZE);
+	return (read_bytes);
 }
